e02a11bc7f1e69d7cbdec06f41e8b6cdc4a4bcfa
Alan Hoffer <alanhoffer97@hotmail.com>
2023-03-29 20:16:19 -0300

    Added navbar component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {  RouterView } from 'vue-router'
+import NavbarVue from './components/Navbar.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
+  
+  <NavbarVue />
 
   <RouterView />
 
---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <main>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+</main>
+</template>
+
+<style scoped></style>
