7037980895472728d9566bfb57b93b34d1121520
Francisco Guerrero <franeguerrero@gmail.com>
2023-04-12 18:48:16 -0300

    update frontend line collection

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -31,7 +31,7 @@ fetch('https://6435d85e537112453fe08ab1.mockapi.io/Home')
   })
 
 
-  
+
 
 
 function togglePage(direction: string): Number {
@@ -56,15 +56,15 @@ const getPageFormated = (page: number) => {
 
 <template>
   <main>
-      <section class="slider" v-if="!sliderState.isLoading">
-        <img id="productImage" :src="newArticles[sliderState.actualPage].image" />
-        
-        <div class="colection">
-          <p>{{ newArticles[sliderState.actualPage].season.toLocaleUpperCase() + ' COLECTION'}}</p>
-          <span></span>
-        </div>
-        <div class="sliderPage"> {{ getPageFormated(sliderState.actualPage) }} </div>
-      </section>
+    <section class="slider" v-if="!sliderState.isLoading">
+      <img id="productImage" :src="newArticles[sliderState.actualPage].image" />
+
+      <div class="colection">
+        <p>{{ newArticles[sliderState.actualPage].season.toLocaleUpperCase() + ' COLECTION' }}</p>
+        <span></span>
+      </div>
+      <div class="sliderPage"> {{ getPageFormated(sliderState.actualPage) }} </div>
+    </section>
 
   </main>
 
@@ -74,64 +74,63 @@ const getPageFormated = (page: number) => {
 
 
 <style scoped>
-.slider{
+.slider {
   height: 100vh;
   width: 81vw;
   overflow: hidden;
   background-color: red;
 }
 
-#productImage{
+#productImage {
   width: 100%;
   height: 100%;
-  position:relative;
+  position: relative;
   object-fit: cover;
-  
+
 }
 
 
-.colection{
-  position: relative;
-  width: 100%;
-  bottom:40px;
-  margin:auto;
+.colection {
   position: absolute;
-  color:white;
-  user-select: none;
-}
-.colection p{
-  position: relative;
-  overflow: hidden;
+  bottom: 20px;
+  left: 100px;
+  margin: 0;
+  color: white;
+  width: 15vw;
   text-align: center;
-  margin-left: 100px;
-  overflow:hidden; 
-  padding: 0px 20px;
-  
-  z-index:1;
-  width:fit-content;
 }
-.colection span{
+
+.colection:before,
+.colection:after {
+  content: "";
   position: absolute;
   top: 50%;
-  transform: translate(0%, -50%);
-  display:inline;
-  width: 100%;
-  height: 10%;
-  background-color:white;
+  height: 2px;
+  background-color: white
+}
+
+.colection:before {
+  left: -6vw;
+  width: 6vw;
+}
+
+.colection:after {
+  right: -57vw;
+  width: 57vw;
 }
 
 
-.sliderPage{
+.sliderPage {
   position: absolute;
   top: 50%;
   transform: translate(0%, -50%);
-  right:10px;
+  right: 10px;
   font-size: 128px;
   font-weight: 600;
   font-family: 'Poppins', sans-serif;
-  -webkit-text-fill-color: transparent;   -webkit-text-stroke: 1px;
-  color:white;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke: 1px;
+  color: white;
   user-select: none;
 
-}
-</style>
+}</style>
