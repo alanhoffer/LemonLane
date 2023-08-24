01955281b9ba1b1405079437ae553257018d73f9
Francisco Guerrero <franeguerrero@gmail.com>
2023-08-24 19:30:18 -0300

    Some visual changes

--- vue-project/src/modules/API/Auth.ts
+++ vue-project/src/modules/API/Auth.ts
@@ -17,8 +17,35 @@ export const logout = () => {
 }
 
 export const login = (email:string, password:string) => {
-    router.push('/profile')
-    localStorage.email = email
+    
+    const url = 'http://localhost:3000/auth/login'; // URL del endpoint
+    const data = {email: email, password: password}; // Datos a enviar en la solicitud
+    
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la solicitud');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Procesar la respuesta del servidor
+        console.log(data)
+        localStorage.email = data.email
+        router.push('/profile')
+      })
+      .catch(error => {
+        // Manejar errores de red u otros errores de la solicitud
+        console.error('Error:', error);
+      });
+    
+
 }
 
 export const register = (name:string, lastname:string, email:string, password:string, repassword:string) =>  {
